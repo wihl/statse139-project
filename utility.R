babc719e6bd4242cdd3388d7bb7e3faf77a78d9a
Goodwill Yoga <goodwillyoga@gmail.com>
2015-11-29 12:47:21 -0500

    Added histogram plot for 2010  with count for each agegroup

--- utility.R
+++ utility.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 #Contains some utility R -cdoe
 fname = file.choose() 
 #fname = "/Users/poojasingh/Documents/HStatE139/git/statse139-project2/statse139-project/Previous Boston Marathon study/BAA data.txt"
@@ -23,6 +25,9 @@ dfm.2010 = subset(dfm, (dfm$Year==2010))
 dfm.2011 = subset(dfm, (dfm$Year==2011))
 dfm.2013 = subset(dfm, (dfm$Year==2013))
 
+#Sanity test for year subset
+length(dfm.2010$BibNum) + length(dfm.2011$BibNum) + length(dfm.2013$BibNum)
+
 #5. Split data by agegroup
 dfm.age.18.34 = subset(dfm, (dfm$Age>=18 & dfm$Age<=34))
 dfm.age.35.39 = subset(dfm, (dfm$Age>=35 & dfm$Age<=39))
@@ -36,9 +41,25 @@ dfm.age.70.74 = subset(dfm, (dfm$Age>=70 & dfm$Age<=74))
 dfm.age.75.79 = subset(dfm, (dfm$Age>=75 & dfm$Age<=79))
 dfm.age.80.plus = subset(dfm, (dfm$Age>=80))
 
-#Sanity Test for agegroup subgroups - the sum of lengths of subgroup should equal to length of original data
+#Sanity Test for agegroup subgroups - the sum of lengths of subset should equal to length of original data
 length(dfm.age.18.34$BibNum) + length(dfm.age.35.39$BibNum) + length(dfm.age.40.44$BibNum) + length(dfm.age.45.49$BibNum) +
   length(dfm.age.50.54$BibNum) + length(dfm.age.55.59$BibNum) + length(dfm.age.60.64$BibNum) + length(dfm.age.65.69$BibNum) + 
   length(dfm.age.70.74$BibNum) + length(dfm.age.75.79$BibNum) + length(dfm.age.80.plus$BibNum)
 
 length(dfm$BibNum)
+
+
+
+#Plot histogram
+#hist(dfm.2010$Age, breaks=c(18, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 83), 
+#       main="Age Distribution for 2010", xlab="Age", freq=TRUE)
+
+#Plot histogram
+ggplot(dfm.2010, aes(x=Age)) + 
+  stat_bin(breaks=c(17, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 83), geom="bar", fill="lightblue") +
+  stat_bin(breaks=c(17, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 83), geom="text", cex=3.25, aes(label=..count..), vjust=-0.5) +
+  labs(title="Age Distribution for 2010")
+
+#Sanity test for histogram count labels
+length(dfm.age.18.34$BibNum[dfm.age.18.34$Year==2010])
+length(dfm.age.75.79$BibNum[dfm.age.75.79$Year==2010])
