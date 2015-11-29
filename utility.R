8fae1dab16db8fca65101046a7c4dfccd5c92128
Goodwill Yoga <goodwillyoga@gmail.com>
2015-11-29 14:53:26 -0500

    Supress warnings

--- utility.R
+++ utility.R
@@ -1,24 +1,24 @@
-library(ggplot2)
+suppressWarnings(suppressMessages(library(ggplot2)))
 
 #Contains some utility R -cdoe
 fname = file.choose() 
 #fname = "/Users/poojasingh/Documents/HStatE139/git/statse139-project2/statse139-project/Previous Boston Marathon study/BAA data.txt"
 dfm = read.csv(fname, header=T,sep=" ")
-names(dfm)
+#names(dfm)
 
 #1. Add all times
 times = as.matrix(dfm[,7:15], ncol=9)
 dfm$totaltime = rowSums(times)
 
 #2. Create factor for gender
-factor(dfm$Gender1F2M, ordered=TRUE) #identify sorted levels in input source
+#factor(dfm$Gender1F2M, ordered=TRUE) #identify sorted levels in input source
 dfm$gender = factor(dfm$Gender1F2M, labels=c("female", "male")) 
-levels(dfm$gender) #output levels
+#levels(dfm$gender) #output levels
 
 #3. Create factor for agegroup
-factor(dfm$Age, ordered=TRUE)  #identify sorted levels in input source
+#factor(dfm$Age, ordered=TRUE)  #identify sorted levels in input source
 dfm$agrgroup = cut(dfm$Age, breaks=c(15, 25, 35, 45, 55, 65, 75, 85))
-levels(dfm$agrgroup)
+#levels(dfm$agrgroup)
 
 #4. Split data by year
 dfm.2010 = subset(dfm, (dfm$Year==2010))
@@ -26,7 +26,7 @@ dfm.2011 = subset(dfm, (dfm$Year==2011))
 dfm.2013 = subset(dfm, (dfm$Year==2013))
 
 #Sanity test for year subset
-length(dfm.2010$BibNum) + length(dfm.2011$BibNum) + length(dfm.2013$BibNum)
+#length(dfm.2010$BibNum) + length(dfm.2011$BibNum) + length(dfm.2013$BibNum)
 
 #5. Split data by agegroup
 dfm.age.18.34 = subset(dfm, (dfm$Age>=18 & dfm$Age<=34))
@@ -42,11 +42,11 @@ dfm.age.75.79 = subset(dfm, (dfm$Age>=75 & dfm$Age<=79))
 dfm.age.80.plus = subset(dfm, (dfm$Age>=80))
 
 #Sanity Test for agegroup subgroups - the sum of lengths of subset should equal to length of original data
-length(dfm.age.18.34$BibNum) + length(dfm.age.35.39$BibNum) + length(dfm.age.40.44$BibNum) + length(dfm.age.45.49$BibNum) +
-  length(dfm.age.50.54$BibNum) + length(dfm.age.55.59$BibNum) + length(dfm.age.60.64$BibNum) + length(dfm.age.65.69$BibNum) + 
-  length(dfm.age.70.74$BibNum) + length(dfm.age.75.79$BibNum) + length(dfm.age.80.plus$BibNum)
+#length(dfm.age.18.34$BibNum) + length(dfm.age.35.39$BibNum) + length(dfm.age.40.44$BibNum) + length(dfm.age.45.49$BibNum) +
+#  length(dfm.age.50.54$BibNum) + length(dfm.age.55.59$BibNum) + length(dfm.age.60.64$BibNum) + length(dfm.age.65.69$BibNum) + 
+#  length(dfm.age.70.74$BibNum) + length(dfm.age.75.79$BibNum) + length(dfm.age.80.plus$BibNum)
 
-length(dfm$BibNum)
+#length(dfm$BibNum)
 
 
 
@@ -55,11 +55,13 @@ length(dfm$BibNum)
 #       main="Age Distribution for 2010", xlab="Age", freq=TRUE)
 
 #Plot histogram
-ggplot(dfm.2010, aes(x=Age)) + 
+plot1 = ggplot(dfm.2010, aes(x=Age)) + 
   stat_bin(breaks=c(17, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 83), geom="bar", fill="lightblue") +
   stat_bin(breaks=c(17, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 83), geom="text", cex=3.25, aes(label=..count..), vjust=-0.5) +
   labs(title="Age Distribution for 2010")
 
+suppressWarnings(suppressMessages(print(plot1)))
+
 #Sanity test for histogram count labels
-length(dfm.age.18.34$BibNum[dfm.age.18.34$Year==2010])
-length(dfm.age.75.79$BibNum[dfm.age.75.79$Year==2010])
+#length(dfm.age.18.34$BibNum[dfm.age.18.34$Year==2010])
+#length(dfm.age.75.79$BibNum[dfm.age.75.79$Year==2010])
